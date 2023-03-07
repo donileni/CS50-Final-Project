e743a42bfbb671f52503280e7d2f82089c5a0edc
David Olsson <david.markus.olsson@gmail.com>
2023-03-07 16:29:06 +0100

    Made number of headings dynamic

--- app.py
+++ app.py
@@ -24,16 +24,27 @@ def write_content():
     sub_keyword = user_inputs["subKeyword"]
     text_length = user_inputs["textLength"]
 
-    headings = generate_headings(keyword, sub_keyword)
+    headings = generate_headings(keyword, sub_keyword, text_length)
     content = generate_content(headings)
 
     return jsonify(content)
 
 
-def generate_headings(keyword, sub_keyword):
-    # Gör antalet headings dynamisk
-    headings_prompt = "Generate 2 SEO-compatible headings appertaining to the main keyword '{}' and the sub-keyword '{}'. Return a comma-seperated list.".format(
-        keyword, sub_keyword)
+def generate_headings(keyword, sub_keyword, text_length):
+
+    number_of_headings = 0
+
+    if text_length == "500":
+        number_of_headings = 2
+    elif text_length == "1000":
+        number_of_headings = 4
+    elif text_length == "1500":
+        number_of_headings = 6
+    else:
+        number_of_headings = 8
+
+    headings_prompt = "Generate {} SEO-compatible headings appertaining to the main keyword '{}' and the sub-keyword '{}'. Return a comma-seperated list.".format(
+        number_of_headings, keyword, sub_keyword)
 
     response = openai.Completion.create(
         model="text-davinci-003",
